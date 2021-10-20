ba5b9d1fef6d21a1440ead9ed7e4b8a712fca263
dynexcel-atif <78357305+dynexcel-atif@users.noreply.github.com>
2021-10-20 18:57:42 +0500

    action for update product info

--- de_sale_integration_dest/models/product_product.py
+++ de_sale_integration_dest/models/product_product.py
@@ -12,6 +12,63 @@ class ProductProduct(models.Model):
     _description = 'Product in DB Integration Sale'
     
     
+    def synch_product_info(self):
+        products = self.browse(self.ids)
+        
+        for product in products:
+            if product.source_ref != False:
+                
+                config_rec = self.env['data.migration.config'].search([], order="id desc", limit=1)
+          
+                if config_rec:
+                    host_name = config_rec.url_db_2
+                    database_name = config_rec.name_db_2
+                    user_name = config_rec.username_2
+                    password = config_rec.password_2
+                else:
+                    raise UserError('Connection settings not configured!')
+                          
+                try: 
+                    #db 2
+                    url_odoo132 = host_name
+                    db_odoo132 = database_name
+                    user_name_db_odoo132 = user_name
+                    password_db_odoo132 = password
+                    common_2 = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url_odoo132))
+                    model_2 = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url_odoo132))
+                    version_db_odoo132 = common_2.version()
+                    print('version_db_odoo132---',version_db_odoo132)
+                except:
+                    raise UserError('Wrong connection setting!')
+                  
+                #authentication
+                odoo_132 = common_2.authenticate(db_odoo132, user_name_db_odoo132,password_db_odoo132, {})
+                
+                
+                
+                
+                db1_product_id = model_2.execute_kw(db_odoo132, odoo_132, password_db_odoo132,'product.product','search_read',
+                                                    [[['id','=',product.source_ref]]],
+                                                    {'fields': ['id', 'default_code', 'barcode'], 'limit': 1})
+
+                print('----db1_product_id2----',db1_product_id)
+                if db1_product_id != []:
+                    if db1_product_id[0]:
+                        if db1_product_id[0].get('barcode'):
+                            product.default_code = db1_product_id[0].get('barcode')
+                            product.barcode = db1_product_id[0].get('barcode')
+#                                 barcode_exists = self.search([('barcode','=',db1_product_id[0].get('barcode'))], order="id desc", limit=1)
+                                
+#                                 """for same barcode message"""
+#                                 if barcode_exists:
+#                                     raise UserError('Same barcode exists as '+(barcode_exists.barcode))
+#                                 if not barcode_exists:
+#                                     product.barcode = db1_product_id[0].get('barcode')
+                
+            
+            
+        
+    
     @api.depends('product_tmpl_id')
     def compute_matching_pattern(self):
         for rec in self:
